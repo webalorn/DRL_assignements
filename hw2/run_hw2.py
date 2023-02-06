0012fb54239e2d98059371fdeb4761fc081f5194
Theophane Vallaeys <webalorn@gmail.com>
2023-02-06 16:37:10 +0100

    Optimization of hyperparameters

--- hw2/run_hw2.py
+++ hw2/run_hw2.py
@@ -83,6 +83,7 @@ def main():
 
     parser.add_argument('--save_params', action='store_true')
     parser.add_argument('--action_noise_std', type=float, default=0)
+    parser.add_argument('--data_path', default='data')
 
     args = parser.parse_args()
 
@@ -100,7 +101,7 @@ def main():
 
     logdir_prefix = 'q2_pg_'  # keep for autograder
 
-    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), args.data_path)
 
     if not (os.path.exists(data_path)):
         os.makedirs(data_path)
--- hw2/run_hw2.sh
+++ hw2/run_hw2.sh
@@ -1,2 +1,15 @@
+
+# Experiment 1 (CartPole)
+
 python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 \
     -dsa --exp_name q1_sb_no_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 \
+    -rtg -dsa --exp_name q1_sb_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 \
+    -rtg --exp_name q1_sb_rtg_na
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 \
+    -dsa --exp_name q1_lb_no_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 \
+    -rtg -dsa --exp_name q1_lb_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 \
+    -rtg --exp_name q1_lb_rtg_na
